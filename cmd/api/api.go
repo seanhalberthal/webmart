b17b50ceec0bffe3e9857a2e3b8d03d7c924f52d
bssmnt <socuting@gmail.com>
2025-03-05 14:10:00 +0000

    init commit 2

---
+++ cmd/api/api.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", app.healthCheckHandler)
+
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("listening on %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
