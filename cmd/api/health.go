c1770d11234d6c9d00c080b761606f2685455f21
bssmnt <socuting@gmail.com>
2025-04-08 09:11:40 +0100

    added some swagger comment blocks for health handler

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -4,6 +4,15 @@ import (
 	"net/http"
 )
 
+// HealthCheck godoc
+//
+//	@Summary		Health check
+//	@Description	Returns API status, environment, and version
+//	@Tags			ops
+//	@Produce		json
+//	@Success		200	{object}	map[string]string
+//	@Failure		500	{object}	error
+//	@Router			/healthcheck [get]
 func (app *application) healthCheckHandler(w http.ResponseWriter, _ *http.Request) {
 	data := map[string]string{
 		"status":  "ok",
